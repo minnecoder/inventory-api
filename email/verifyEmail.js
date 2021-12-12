6150e9cfb8c9cda0b5839ab2af45c1a3e84e683e
minnecoder <sdparkin@protonmail.com>
2021-12-12 10:57:41 -0600

    Added email verification Added to both user registration controllers

---
+++ email/verifyEmail.js
@@ -0,0 +1,26 @@
+const crypto = require('crypto')
+
+const { ROOT_DOMAIN, JWT_SIGNATURE } = process.env
+
+exports.createVerifyEmailToken = async (email) => {
+    try {
+        const authString = `${JWT_SIGNATURE}:${email}`
+        return crypto.createHash('sha256').update(authString).digest('hex')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+exports.createVerifyEmailLink = async (email) => {
+    try {
+        // Create token
+        const emailToken = await this.createVerifyEmailToken(email)
+        // Encode url string
+        const URIencodedEmail = encodeURIComponent(email)
+        // Return verification link
+        // TODO make front end route to handle this link
+        return `https://${ROOT_DOMAIN}/verify/${URIencodedEmail}/${emailToken}`
+    } catch (error) {
+        console.log(error)
+    }
+}
