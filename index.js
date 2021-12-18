5719b040c797b6d4de96b3cfb0631b7ce4a79a55
minnecoder <sdparkin@protonmail.com>
2021-12-18 09:39:32 -0600

    .

--- index.js
+++ index.js
@@ -10,29 +10,12 @@ const app = express();
 
 
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 dotenv.config({ path: './config/config.env' });
 app.use(bodyParser.json());
 app.use(cookieParser())
 
-app.use((req, res, next) => {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 
 // DB Connection and Test
 const db = require('./config/postgres-db');
