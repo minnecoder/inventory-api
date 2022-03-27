da9ecd5f298b86401058d13b2473ccabd297ab3e
minnecoder <sdparkin@protonmail.com>
2022-03-26 21:32:59 -0500

    Fixed TODO item

--- models/index.model.ts
+++ models/index.model.ts
@@ -9,7 +9,6 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js');
 const db: any = {};
 
-// TODO fix type for sequelize
 let sequelize: any;
 if (config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable], config);
