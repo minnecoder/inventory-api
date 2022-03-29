e8e7c9aa21731f2c89fea46eafa3c307db679d51
minnecoder <sdparkin@protonmail.com>
2022-03-28 20:59:30 -0500

    Fixed errors with create and update services

--- services/productSupplier.service.ts
+++ services/productSupplier.service.ts
@@ -1,11 +1,15 @@
 import DB from "../config/postgres-db";
 import { CreateProductSupplierDTO } from "../dtos/productSupplier.dto";
 import { ProductSupplier } from "../interfaces/productSupplier.interface";
+import { Product } from "../interfaces/product.interface";
+import { Supplier } from "../interfaces/supplier.interface";
 import { NotFound } from "../errors/NotFound";
 import { AlreadyExists } from "../errors/AlreadyExists";
 
 class ProductSupplierService {
     public ProductSupplier = DB.ProductSupplier
+    public Product = DB.Product
+    public Supplier = DB.Supplier
 
     public async findAllProductSuppliers(): Promise<ProductSupplier[]> {
         const allProductSuppliers: ProductSupplier[] = await this.ProductSupplier.findAll()
@@ -26,11 +30,11 @@ class ProductSupplierService {
         if (findProductSupplier) throw new AlreadyExists('ProductSupplier')
 
         // Check if productId is valid
-        const findProduct: ProductSupplier = await this.ProductSupplier.findOne({ where: { id: ProductSupplierData.productId } })
+        const findProduct: Product = await this.Product.findOne({ where: { id: ProductSupplierData.productId } })
         if (!findProduct) throw new NotFound('Product')
 
         // Check if supplierId is valid
-        const findSupplier: ProductSupplier = await this.ProductSupplier.findOne({ where: { id: ProductSupplierData.supplierId } })
+        const findSupplier: Supplier = await this.Supplier.findOne({ where: { id: ProductSupplierData.supplierId } })
         if (!findSupplier) throw new NotFound('Supplier')
 
         const createdProductSupplierData: ProductSupplier = await this.ProductSupplier.create({ ...ProductSupplierData })
@@ -44,11 +48,11 @@ class ProductSupplierService {
         if (!findProductSupplier) throw new NotFound('ProductSupplier')
 
         // Check if productId is valid
-        const findProduct: ProductSupplier = await this.ProductSupplier.findOne({ where: { id: ProductSupplierData.productId } })
+        const findProduct: Product = await this.Product.findOne({ where: { id: ProductSupplierData.productId } })
         if (!findProduct) throw new NotFound('Product')
 
         // Check if supplierId is valid
-        const findSupplier: ProductSupplier = await this.ProductSupplier.findOne({ where: { id: ProductSupplierData.supplierId } })
+        const findSupplier: Supplier = await this.Supplier.findOne({ where: { id: ProductSupplierData.supplierId } })
         if (!findSupplier) throw new NotFound('Supplier')
 
         await this.ProductSupplier.update({ ...ProductSupplierData }, { where: { id: ProductSupplierId } })
